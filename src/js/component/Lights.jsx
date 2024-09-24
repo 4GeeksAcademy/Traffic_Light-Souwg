e6278b673164acaa156386cd0b8ceda5d6f1ce1e
Souwg <sansanvzla@gmail.com>
2024-09-24 00:41:20 +0000

    Traffic_lights_

--- src/js/component/Lights.jsx
+++ src/js/component/Lights.jsx
@@ -2,15 +2,19 @@ import React, { useState } from "react"
 
 const TrafficLights = () =>{
     const[color, setColor] = useState('red')
+    const[colorPurple, setcoloPurple] = useState('purple')
     const[lights, setLights] = useState(['red', 'yellow', 'green'])
 
     return(
         <>
-        <div className="card m-auto" style={{height: "80px", width: "30px",
-        background: "black",
-        borderRadius: "0",
-        }}>
-        </div>
+            <div className="card m-auto"
+            style={{
+                height: "60px", 
+                width: "30px",
+                background: "black",
+                borderRadius: "0",
+                }}>
+            </div>
         <div className="card d-flex justify-content-center align-items-center m-auto mt-0" 
             style={{
             background: "black",
@@ -25,19 +29,20 @@ const TrafficLights = () =>{
                 <button
                 key={index} 
                 onClick={()=>setColor(light)} 
-                className={`btn light ${ light == color ? "active" : ""}`}
+                className={`btn ${light === color ? "active" : "" || light === colorPurple ? "active" : ""}`}
                 style={{
                     backgroundColor: light,
                     width: "6rem",
                     height: "6rem", 
                     margin: "0.5rem 0",
-                    borderRadius: "50%", 
+                    borderRadius: "60%", 
                     cursor: "pointer", 
-                    boxShadow: light === color ? "0px 0px 20px lightyellow" : "none",
+                    boxShadow: light === color ? "0px 0px 20px lightyellow" : "none" || light === colorPurple ? "0px 0px 20px purple" : "none",
                 }}>
                 </button>
             )}
             </div>
+            {/*<button className="btn btn-primary border-0 mb-2">click me!</button>*/}
         </div>
         </>   
     );
